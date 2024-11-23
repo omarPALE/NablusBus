b569550675cc4597f46574ee8bce1a0cc55cf18c
omar <omar.alratrot@yahoo.com>
2024-11-23 18:00:11 +0200

    add documentation file

--- web/client/login/component/signup-component/SignupForm.jsx
+++ web/client/login/component/signup-component/SignupForm.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef } from "react";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import { Button } from "react-bootstrap";
